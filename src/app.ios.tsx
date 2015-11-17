e2825cd0c1e08cb3eee4878218496c6c07bf69c1
Patricio López <lopezjuripatricio@gmail.com>
2015-11-17 20:16:00 -0300

    hello world :)

---
+++ src/app.ios.tsx
@@ -0,0 +1,44 @@
+/// <reference path="../typings/tsd.d.ts"/>
+
+import React from 'react-native'
+const { StyleSheet, Text, View } = React
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+})
+
+
+export default class App extends React.Component<any, any> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.ios.js
+                </Text>
+                <Text style={styles.instructions}>
+                    Press Cmd+R to reload, {'\n'}
+                    Cmd+D or shake for dev menu
+                </Text>
+            </View>
+        )
+    }
+}
