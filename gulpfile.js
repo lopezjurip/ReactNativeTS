77e38253cf5a0d79f19a21722a8e6d14fb67eff0
Patricio López <lopezjuripatricio@gmail.com>
2015-11-17 20:56:42 -0300

    refactor gulpfile and package scripts

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var ts = require('gulp-typescript');
 
 var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('typescript', function() {
+gulp.task('build', function() {
     var tsResult = tsProject.src().pipe(ts(tsProject));
     return tsResult.js.pipe(gulp.dest('built'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./src/**/*.ts', ['typescript']);
-  gulp.watch('./src/**/*.tsx', ['typescript']);
+gulp.task('watch', ['build'], function() {
+  gulp.watch('src/**/*.ts', ['build']);
+  gulp.watch('src/**/*.tsx', ['build']);
 });
 
-gulp.task('default', ['typescript', 'watch']);
+gulp.task('default', ['build']);
