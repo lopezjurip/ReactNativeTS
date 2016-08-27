de93d4d050cdda98a007d8c95279a3197a03780c
Patricio López <lopezjuripatricio@gmail.com>
2016-08-27 09:57:33 -0300

    Add better typings of custom module

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,4 +1,22 @@
+// It is necessary to add the typings of imported oomponents
+
+declare module "react-native-button" {
+    import React, { Component } from "react";
+
+    interface Props {
+        style?: React.ViewStyle;
+        styleDisabled?: React.ViewStyle;
+        onPress?: () => any;
+    }
+
+    export default class Button extends Component<Props, any> {}
+}
+
+/*
+// If you are lazy and don't care about the strict typings:
+
 declare module "react-native-button" {
     const value: any;
     export default value;
 }
+*/
